9eb56e752abe808a0443d85b6d33315a3bc5ea00
VFortier <vincentf26@gmail.com>
2021-06-06 00:03:42 +0100

    FIRST!

---
+++ collisionHelper.js
@@ -0,0 +1,53 @@
+(() => {
+  CollisionHelper = {}
+
+  // From https://stackoverflow.com/questions/57891494/how-to-calculate-intersection-point-of-a-line-on-a-circle-using-p5-js
+  // TODO - Ça check pour une ligne "infinie" qui passe entre deux points - to fix - see inBetween to check for intersect pts in between p1 and p2
+  CollisionHelper.intersectLineCircle = function(p1, p2, cpt, r) {
+
+      let sign = function(x) { return x < 0.0 ? -1 : 1; };
+
+      let x1 = p1.copy().sub(cpt);
+      let x2 = p2.copy().sub(cpt);
+
+      let dv = x2.copy().sub(x1);
+      let dr = dv.mag();
+      let D = x1.x*x2.y - x2.x*x1.y;
+
+      // evaluate if there is an intersection
+      let di = r*r*dr*dr - D*D;
+      if (di < 0.0)
+          return [];
+
+      let t = sqrt(di);
+
+      ip = [];
+      ip1 = new p5.Vector(D*dv.y + sign(dv.y)*dv.x * t, -D*dv.x + abs(dv.y) * t).div(dr*dr).add(cpt)
+
+      if (CollisionHelper.ptsInBetween(p1, p2, ip1)) {
+        ip.push( ip1 );
+      }
+
+      if (di > 0.0) {
+        ip2 = new p5.Vector(D*dv.y - sign(dv.y)*dv.x * t, -D*dv.x - abs(dv.y) * t).div(dr*dr).add(cpt)
+
+        if (CollisionHelper.ptsInBetween(p1, p2, ip1)) {
+          ip.push( ip2 );
+        }
+      }
+      return ip;
+  }
+
+  CollisionHelper.ptsInBetween = function(p1, p2, px) {
+
+    let v = p2.copy().sub(p1);
+    let d = v.mag();
+    v = v.normalize();
+
+    let vx = px.copy().sub(p1);
+    let dx = v.dot(vx);
+
+    return dx >= 0 && dx <= d;
+  }
+
+})()
---
+++ raceOld.js
@@ -0,0 +1,59 @@
+function RaceOld() {
+  this.START_POS = createVector(2700, 3600)
+  this.RACE_PTS = [
+      createVector(2500,3700),
+      createVector(2500,2000),
+
+      createVector(3300,2000),
+      createVector(3300,1000),
+
+      createVector(3800,1000),
+      createVector(3800,2800),
+
+      createVector(2900,2800),
+
+
+      createVector(2900,3700),
+      createVector(2500,3700),
+    ]
+
+
+  this.RACE_DEFINITION = []
+
+  this.getStartPosition = function() {
+    return this.START_POS
+  }
+
+  this.getRacePts = function() {
+    return this.RACE_PTS
+  }
+
+  this._drawBg = function() {
+    strokeWeight(1)
+    for (var i=0; i<ENV_W; i += 20) {
+      stroke(100)
+      line(i,0,i,ENV_H)
+    }
+
+    for (var i=0; i<ENV_H; i += 20) {
+      stroke(100)
+      line(0,i,ENV_W,i)
+    }
+  }
+
+  this.draw = function() {
+    this._drawBg()
+
+    stroke(0)
+    strokeWeight(3)
+
+    previousPoint = null
+    this.RACE_PTS.forEach(point => {
+      if (previousPoint) {
+        line(previousPoint.x, previousPoint.y, point.x, point.y)
+      }
+      
+      previousPoint = point
+    })
+  }
+}
